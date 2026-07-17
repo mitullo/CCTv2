608465259e9c2f28b9423d21ade3dd2a07380570
mitullo <maximzaitov@gmail.com>
2026-07-17 14:48:08 +0500

    Update voices.js

--- audio/voices.js
+++ audio/voices.js
@@ -14,5 +14,13 @@ window.CCT_VOICE_LIBRARY={
   siri4:{
     label:"Siri 4",
     basePath:"audio/siri4"
+  },
+  russianmale:{
+    label:"Русский — мужской",
+    basePath:"audio/russian-male"
+  },
+  russianfemale:{
+    label:"Русский — женский",
+    basePath:"audio/russian-female"
   }
 };
